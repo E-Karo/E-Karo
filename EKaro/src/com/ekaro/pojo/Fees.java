842899ba2bd92aa014e4f7410d0af30f69ba5d22
Titus Chirchir <titus.chirchir12@gmail.com>
2014-04-20 18:15:36 -0400

    Adding Fees POJO

---
+++ EKaro/src/com/ekaro/pojo/Fees.java
@@ -0,0 +1,24 @@
+package com.ekaro.pojo;
+
+public class Fees {
+	
+	private double amount;
+	private double description;
+	
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public double getDescription() {
+		return description;
+	}
+
+	public void setDescription(double description) {
+		this.description = description;
+	}
+	
+}
