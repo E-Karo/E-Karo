ebee911c7fb13717396310dc2853aba990c91d5a
Titus Chirchir <titus.chirchir12@gmail.com>
2014-04-20 18:52:26 -0400

    Modify Fees

--- EKaro/src/com/ekaro/pojo/Fees.java
+++ EKaro/src/com/ekaro/pojo/Fees.java
@@ -3,22 +3,9 @@ package com.ekaro.pojo;
 public class Fees {
 	
 	private double amount;
-	private double description;
-	
-	public double getAmount() {
-		return amount;
-	}
+	private String description;
+	private int year;
+	private byte term;
+	private SchoolYear form;
 
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
-
-	public double getDescription() {
-		return description;
-	}
-
-	public void setDescription(double description) {
-		this.description = description;
-	}
-	
 }
