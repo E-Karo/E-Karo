3773fb1026ee3e1aa8c2176a6079110e3409b36f
Titus Chirchir <titus.chirchir12@gmail.com>
2014-04-20 20:35:27 -0400

    Where

--- EKaro/src/com/ekaro/pojo/Fees.java
+++ EKaro/src/com/ekaro/pojo/Fees.java
@@ -10,5 +10,35 @@ public class Fees {
 	private int year;
 	private Semester semester;
 	private SchoolYear schoolYear;
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public Semester getSemester() {
+		return semester;
+	}
+	public void setSemester(Semester semester) {
+		this.semester = semester;
+	}
+	public SchoolYear getSchoolYear() {
+		return schoolYear;
+	}
+	public void setSchoolYear(SchoolYear schoolYear) {
+		this.schoolYear = schoolYear;
+	}
 
 }
